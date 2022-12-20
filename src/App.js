46056716d72576293fdcf703e21a247b1e29434a
DeibyOd <deibyod@gmail.com>
2022-12-20 14:20:29 -0300

    Professional and personal sites blocks

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ import Footer from './components/Footer/Footer';
 
 import './App.css';
 
+import PersonalSpacesContent from './data/PersonalBoxesContent.json';
+import ProfessionalSpacesContent from './data/ProfessionalBoxesContent.json';
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header />
         <Opening />
-        <Boxes />
+        <h1>PROFESIONAL</h1>
+        <Boxes boxContent={ProfessionalSpacesContent} />
+        <h1>PERSONAL</h1>
+        <Boxes boxContent={PersonalSpacesContent} />
         <SectionTitle />
         <Map />
         <Footer />
--- src/components/Boxes/Boxes.js
+++ src/components/Boxes/Boxes.js
@@ -1,14 +1,33 @@
-import React from 'react';
+/*import React from 'react';
 import Box from '../Box/Box';
 import './boxes.scss';
-import SpacesContent from '../../data/BoxesContent.json';
 
 const Boxes = ()  => {
+    const {SpacesContent} = this.props;
+
     return(
         <div className="boxes-container">
             {SpacesContent.map((singleBox) => (<Box key={singleBox.id} box={singleBox} />))}
         </div>
     )
+}
+
+export default Boxes;*/
+
+import React, { Component } from 'react';
+import Box from '../Box/Box';
+import './boxes.scss';
+class Boxes extends Component {
+
+    render() {
+        const {boxContent} = this.props;
+
+        return(
+            <div className="boxes-container">
+                {boxContent.map((singleBox) => (<Box key={singleBox.id} box={singleBox} />))}
+            </div>
+        )
+    }
 }
 
 export default Boxes;
