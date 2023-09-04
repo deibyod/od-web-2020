b9c9b1fd7447696a9c84da8b2510d2d9df8b874c
DeibyOd <deibyod@gmail.com>
2023-09-04 15:59:02 -0300

    Personal photo variations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './header.scss';
+import PersonalPhoto0 from './images/photos/deibyod-0.png';
 import PersonalPhoto1 from './images/photos/deibyod-1.png';
 import PersonalPhoto2 from './images/photos/deibyod-2.png';
 import PersonalPhoto3 from './images/photos/deibyod-3.png';
@@ -8,6 +9,7 @@ import PersonalPhoto5 from './images/photos/deibyod-5.png';
 import PersonalPhoto6 from './images/photos/deibyod-6.png';
 
 const PersonalPhoto = [
+    PersonalPhoto0,
     PersonalPhoto1,
     PersonalPhoto2,
     PersonalPhoto3,
@@ -26,7 +28,7 @@ const Header = ()  => {
     return(
         <div className="header-container">
             <a href="http://www.deibyod.co" rel="noopener noreferrer">
-                <img className="personal-photo" src={PersonalPhoto[getRandomInt(1, 6)]} alt="Deiby Od profile" />
+                <img className="personal-photo" src={PersonalPhoto[getRandomInt(0, 6)]} alt="Deiby Od profile" />
             </a>
             <p className="name-style"><strong>Deiby</strong> Fabian <strong>O</strong>rdoñez <strong>D</strong>iaz</p>
         </div>
